4f58ca2dc4a001cb8944a2379e43b09e5a702d93
blacksevenzqj <397353721@qq.com>
2018-03-22 23:10:59 +0800

    home

--- elastic-search/src/test/java/com/leno/elasticSearch/test/TestIndex.java
+++ elastic-search/src/test/java/com/leno/elasticSearch/test/TestIndex.java
@@ -1,24 +1,36 @@
 package com.leno.elasticSearch.test;
 
 import com.leno.elasticSearch.ESClientFactory;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
 
 public class TestIndex {
 
-    public static void main() throws Exception{
-        createIndex();
+    public static void main(String[] args) throws Exception{
+//        createIndex();
+        getIndex();
+
 
         close();
     }
 
     public static void createIndex() throws Exception{
         RestHighLevelClient restHighLevelClient = ESClientFactory.getHighLevelClient();
-
         IndexRequest indexRequest = new IndexRequest("xiamo", "school", "1")
-                .source("articleID", "1111");
-
+                .source("articleID", "qwe1123ghf",
+                        "userID", "19",
+                        "hidden", "false",
+                        "postDate", "2018-03-21"
+                );
         restHighLevelClient.index(indexRequest);
+    }
+
+    public static void getIndex() throws Exception{
+        RestHighLevelClient restHighLevelClient = ESClientFactory.getHighLevelClient();
+        GetRequest getRequest = new GetRequest("xiamo", "school", "1");
+        System.out.println(restHighLevelClient.exists(getRequest));
     }
 
     public static void close() throws Exception{
